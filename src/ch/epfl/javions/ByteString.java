db4882bb0593c84c81948c2ad9385ece9def3cee
Yshai <yshai.dinee-baumgarten@epfl.ch>
2023-02-22 14:52:50 +0100

    Commit

--- src/ch/epfl/javions/ByteString.java
+++ src/ch/epfl/javions/ByteString.java
@@ -3,11 +3,17 @@ package ch.epfl.javions;/*
  *	Date:        21/02/23
  */
 
+import java.util.Arrays;
+import java.util.HexFormat;
+import java.util.Objects;
+
 public final class ByteString
 {
+    private byte [] bytes;
+    private final static HexFormat hf = HexFormat.of().withUpperCase();
     public ByteString(byte[] bytes)
     {
-
+        this.bytes = bytes.clone();
     }
 
     public static ByteString ofHexadecimalString(String hexString)
@@ -16,5 +22,59 @@ public final class ByteString
         {
             throw new IllegalArgumentException();
         }
+
+        byte[] bytes = hf.parseHex(hexString);
+
+        return new ByteString(bytes);
+    }
+
+    public int size()
+    {
+        return bytes.length;
+    }
+    public int byteAt(int index)
+    {
+        if (index < 0 || index > (bytes.length -1))
+        {
+            throw new IndexOutOfBoundsException();
+        }
+
+        return bytes[index];
+    }
+    public long bytesInRange(int fromIndex, int toIndex)
+    {
+        Objects.checkFromToIndex(fromIndex, toIndex, this.size());
+
+        if (toIndex - fromIndex >= Long.SIZE)
+        {
+            throw new IllegalArgumentException();
+        }
+
+        long mask = bytes[fromIndex];
+
+        for (int i = fromIndex + 1; i < toIndex; i++)
+        {
+            mask = ((mask << 8) | bytes[i]);
+        }
+
+        return mask;
+    }
+
+    @Override
+    public boolean equals(Object obj)
+    {
+        return (obj instanceof ByteString) && (Arrays.equals(((ByteString) obj).bytes, this.bytes ));
+    }
+
+    @Override
+    public int hashCode()
+    {
+        return Arrays.hashCode(bytes);
+    }
+
+    @Override
+    public String toString()
+    {
+        return hf.formatHex(bytes);
     }
 }
