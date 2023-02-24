2d1938c84e89a4223e7ed796c997f97ee25c1452
Yshai <yshai.dinee-baumgarten@epfl.ch>
2023-02-24 13:23:22 +0100

    Commit

--- src/ch/epfl/cs108/Submit.java
+++ src/ch/epfl/cs108/Submit.java
@@ -34,7 +34,7 @@ public final class Submit {
     // Jeton du premier membre du groupe
     private static final String TOKEN_1 = "ahquah0I";
     // Jeton du second membre (identique au premier pour les personnes travaillant seules)
-    private static final String TOKEN_2 = "";
+    private static final String TOKEN_2 = "Keeb3xao";
     // -------------
 
     private static final String ZIP_ENTRY_NAME_PREFIX = "Javions/";
