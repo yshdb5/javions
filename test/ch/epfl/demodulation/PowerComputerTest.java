df15f6a1ab06a3b9c0a811f4c0e42c4f7c815816
Yshai <yshai.dinee-baumgarten@epfl.ch>
2023-03-05 19:35:58 +0100

    Commit

--- test/ch/epfl/demodulation/PowerComputerTest.java
+++ test/ch/epfl/demodulation/PowerComputerTest.java
@@ -12,12 +12,10 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class PowerComputerTest
 {
-
     @Test
-    void SamplesDecoderWorksOnKnownValues() throws IOException
+    void PowerComputerOnKnownValues() throws IOException
     {
         FileInputStream stream = new FileInputStream("resources/samples.bin");
-
         PowerComputer powerComputer =  new PowerComputer(stream, 1208);
 
         int [] powerTab = new int[1208];
@@ -25,5 +23,50 @@ class PowerComputerTest
         powerComputer.readBatch(powerTab);
 
         System.out.println("PowerTab: " + Arrays.toString(powerTab));
+    }
+
+    @Test
+    void PowerComputerThrowsIllegalArgumentException() throws IOException
+    {
+        FileInputStream stream = new FileInputStream("resources/samples.bin");
+
+        assertThrows(IllegalArgumentException.class, () -> new PowerComputer(stream, 5));
+        assertThrows(IllegalArgumentException.class, () -> new PowerComputer(stream, 0));
+    }
+
+    @Test
+    void readbatchThrowsIllegalArgumentException() throws IOException
+    {
+        FileInputStream stream = new FileInputStream("resources/samples.bin");
+        PowerComputer powerComputer =  new PowerComputer(stream, 1208);
+        int [] shortTab = new int[10];
+
+        assertThrows(IllegalArgumentException.class, () -> powerComputer.readBatch(shortTab));
+    }
+
+    @Test
+    void readbatchReturnsTheNumberOfEchantillonsRead() throws IOException
+    {
+        FileInputStream stream = new FileInputStream("resources/samples.bin");
+        PowerComputer powerComputer =  new PowerComputer(stream, 1208);
+        int [] shortTab = new int[1208];
+
+        int actual = powerComputer.readBatch(shortTab);
+        int expected = 1201;
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void readbatchReturnsTheNumberOfEchantillonsRead2() throws IOException
+    {
+        FileInputStream stream = new FileInputStream("resources/samples.bin");
+        PowerComputer powerComputer =  new PowerComputer(stream, 56);
+        int [] shortTab = new int[56];
+
+        int actual = powerComputer.readBatch(shortTab);
+        int expected = 56;
+
+        assertEquals(expected, actual);
     }
 }
--- test/ch/epfl/demodulation/SamplesDecoderTest.java
+++ test/ch/epfl/demodulation/SamplesDecoderTest.java
@@ -10,13 +10,12 @@ import java.util.Arrays;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class SamplesDecoderTest {
-
+class SamplesDecoderTest
+{
     @Test
     void SamplesDecoderWorksOnKnownValues() throws IOException
     {
         FileInputStream stream = new FileInputStream("resources/samples.bin");
-
         SamplesDecoder decoder =  new SamplesDecoder(stream, 16);
 
         short [] shortTab = new short[16];
@@ -24,5 +23,56 @@ class SamplesDecoderTest {
         decoder.readBatch(shortTab);
 
         System.out.println("Decoder Tab: " +Arrays.toString(shortTab));
+    }
+
+    @Test
+    void SamplesDecoderThrowsIllegalArgumentException() throws IOException
+    {
+        FileInputStream stream = new FileInputStream("resources/samples.bin");
+
+        assertThrows(IllegalArgumentException.class, () -> new SamplesDecoder(stream, -1));
+        assertThrows(IllegalArgumentException.class, () -> new SamplesDecoder(stream, 0));
+    }
+
+    @Test
+    void SamplesDecoderThrowsNullPointerException()
+    {
+        FileInputStream stream = null;
+
+        assertThrows(NullPointerException.class, () -> new SamplesDecoder(stream, 1));
+    }
+
+    @Test
+    void readbatchThrowsIllegalArgumentException() throws IOException
+    {
+        FileInputStream stream = new FileInputStream("resources/samples.bin");
+        SamplesDecoder decoder =  new SamplesDecoder(stream, 16);
+        short [] shortTab = new short[10];
+
+        assertThrows(IllegalArgumentException.class, () -> decoder.readBatch(shortTab));
+    }
+
+    @Test
+    void readbatchReturnsTheNumberOfEchantillonsRead() throws IOException
+    {
+        FileInputStream stream = new FileInputStream("resources/samples.bin");
+        SamplesDecoder decoder =  new SamplesDecoder(stream, 16);
+        short [] shortTab = new short[16];
+        int actual = decoder.readBatch(shortTab);
+        int expected = 16;
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void readbatchReturnsTheNumberOfEchantillonsRead2() throws IOException
+    {
+        FileInputStream stream = new FileInputStream("resources/samples.bin");
+        SamplesDecoder decoder =  new SamplesDecoder(stream, 5000);
+        short [] shortTab = new short[5000];
+        int actual = decoder.readBatch(shortTab);
+        int expected = 2402;
+
+        assertEquals(expected, actual);
     }
 }
