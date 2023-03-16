b1ccefbb1dfd2a83a928d5dd20b440b52b09cfa4
Yshai <yshai.dinee-baumgarten@epfl.ch>
2023-03-16 11:55:39 +0100

    Commit

--- test/ch/epfl/adsb/AirbornePositionMessageTest.java
+++ test/ch/epfl/adsb/AirbornePositionMessageTest.java
@@ -1,16 +1,13 @@
 package ch.epfl.adsb;
 
-import ch.epfl.javions.GeoPos;
-import ch.epfl.javions.adsb.CprDecoder;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class CprDecoderTest
-{
+class AirbornePositionMessageTest {
 
     @Test
-    void decodePositionWorks()
+    void of()
     {
 
     }
