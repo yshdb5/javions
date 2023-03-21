0e83867f9cccde12b0c6b5be023c74fee6172091
Yshai <yshai.dinee-baumgarten@epfl.ch>
2023-03-21 09:25:36 +0100

    Commit

--- src/ch/epfl/javions/adsb/AirborneVelocityMessage.java
+++ src/ch/epfl/javions/adsb/AirborneVelocityMessage.java
@@ -2,6 +2,7 @@ package ch.epfl.javions.adsb;
 
 import ch.epfl.javions.Bits;
 import ch.epfl.javions.Preconditions;
+import ch.epfl.javions.Units;
 import ch.epfl.javions.aircraft.IcaoAddress;
 
 import java.util.Objects;
@@ -23,17 +24,71 @@ public record AirborneVelocityMessage(long timeStampNs, IcaoAddress icaoAddress,
         {
             return null;
         }
+        double speedAngle_RADIAN;
+        double speedNorm_METER_PER_SECOND;
 
         if ((ST == 1) || (ST == 2))
         {
             int directionEW = Bits.extractUInt(rawMessage.payload(), 42, 1);
-            int speedEW = Bits.extractUInt(rawMessage.payload(), 32, 10);
+            int speedEW = Bits.extractUInt(rawMessage.payload(), 32, 10) - 1;
             int directionNS = Bits.extractUInt(rawMessage.payload(), 31, 1);
-            int speedNS = Bits.extractUInt(rawMessage.payload(), 21, 10);
+            int speedNS = Bits.extractUInt(rawMessage.payload(), 21, 10) - 1;
+
+            if ((speedEW == -1) || (speedNS == -1))
+            {
+                return null;
+            }
+
+            if (directionNS == 0 && directionEW == 0)
+            {
+                speedAngle_RADIAN = Math.atan2(-speedNS, -speedEW);
+            }
+            else if (directionNS == 1 && directionEW == 0)
+            {
+                speedAngle_RADIAN = Math.atan2(speedNS, -speedEW);
+            }
+            else if (directionNS == 0 && directionEW == 1)
+            {
+                speedAngle_RADIAN = Math.atan2(-speedNS, speedEW);
+            }
+            else
+            {
+                speedAngle_RADIAN = Math.atan2(speedNS, speedEW);
+            }
+
+            if (ST == 1)
+            {
+                speedNorm_METER_PER_SECOND = Units.convertFrom(Math.hypot((speedNS), (speedEW)), Units.Speed.KNOT);
+            }
+            else
+            {
+                speedNorm_METER_PER_SECOND = Units.convertFrom(Math.hypot((speedNS), (speedEW))*4, Units.Speed.KNOT);
+            }
+            return new AirborneVelocityMessage(rawMessage.timeStampNs(), rawMessage.icaoAddress(), speedNorm_METER_PER_SECOND, speedAngle_RADIAN);
         }
 
+        else
+        {
+            int SH = Bits.extractUInt(rawMessage.payload(), 42, 1);
 
+            if (SH == 0)
+            {
+                return null;
+            }
+            else
+            {
+                speedAngle_RADIAN = Units.convertFrom(Bits.extractUInt(rawMessage.payload(), 32, 10)*Math.scalb(1, -10), Units.Angle.TURN);
 
-        return new AirborneVelocityMessage(rawMessage.timeStampNs(), rawMessage.icaoAddress(), 1, 1);
+                if (ST == 3)
+                {
+                    speedNorm_METER_PER_SECOND = Units.convertFrom(Bits.extractUInt(rawMessage.payload(), 32, 10), Units.Speed.KNOT);
+                }
+                else
+                {
+                    speedNorm_METER_PER_SECOND = Units.convertFrom(Bits.extractUInt(rawMessage.payload(), 32, 10)*4, Units.Speed.KNOT);
+                }
+                return new AirborneVelocityMessage(rawMessage.timeStampNs(), rawMessage.icaoAddress(), speedNorm_METER_PER_SECOND, speedAngle_RADIAN);
+            }
+        }
     }
 }
