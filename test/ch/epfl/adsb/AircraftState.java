f98328ed95cee7dfc088126d856fdce0b4c75f2c
Yshai <yshai.dinee-baumgarten@epfl.ch>
2023-03-22 14:54:57 +0100

    Commit

--- test/ch/epfl/adsb/AircraftState.java
+++ test/ch/epfl/adsb/AircraftState.java
@@ -11,43 +11,44 @@ public class AircraftState implements AircraftStateSetter
 {
 
     @Override
-    public void setLastMessageTimeStampNs(long timeStampNs) {
-
+    public void setLastMessageTimeStampNs(long timeStampNs)
+    {
+        System.out.println("time stamp : " + timeStampNs);
     }
 
     @Override
     public void setCategory(int category)
     {
-
+        System.out.println("catégorie : " + category);
     }
 
     @Override
     public void setCallSign(CallSign callSign)
     {
-
+        System.out.println("indicatif : " + callSign);
     }
 
     @Override
     public void setPosition(GeoPos position)
     {
-
+        System.out.println("position : " + position);
     }
 
     @Override
     public void setAltitude(double altitude)
     {
-
+        System.out.println("altitude : " + altitude);
     }
 
     @Override
     public void setVelocity(double velocity)
     {
-
+        System.out.println("vélocité : " + velocity);
     }
 
     @Override
     public void setTrackOrHeading(double trackOrHeading)
     {
-
+        System.out.println("Track or heading : " + trackOrHeading);
     }
 }
