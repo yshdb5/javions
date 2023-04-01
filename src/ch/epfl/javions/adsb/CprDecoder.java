8963af31da54490f194fa4076d1ce0b37c1e2b09
Yshai <yshai.dinee-baumgarten@epfl.ch>
2023-04-01 18:36:27 +0200

    Commit

--- src/ch/epfl/javions/adsb/CprDecoder.java
+++ src/ch/epfl/javions/adsb/CprDecoder.java
@@ -35,16 +35,8 @@ public class CprDecoder {
         Preconditions.checkArgument((mostRecent == 1) || (mostRecent == 0));
 
         int zonePhi = (int) Math.rint(y0 * Z1 - y1 * Z0);
-        int zonePhi0;
-        int zonePhi1;
-
-        if (zonePhi < 0) {
-            zonePhi0 = zonePhi + Z0;
-            zonePhi1 = zonePhi + Z1;
-        } else {
-            zonePhi0 = zonePhi;
-            zonePhi1 = zonePhi;
-        }
+        int zonePhi0 = (zonePhi < 0) ? zonePhi + Z0 : zonePhi;
+        int zonePhi1 = (zonePhi < 0) ? zonePhi + Z1 : zonePhi;
 
         double latitude0Turn = recenterPosition(DELTA0 * (zonePhi0 + y0));
         double latitude1Turn = recenterPosition(DELTA1 * (zonePhi1 + y1));
@@ -70,39 +62,19 @@ public class CprDecoder {
         int zoneNumber1 = zoneNumber00 - 1;
 
         int zoneLambda = (int) Math.rint(x0 * zoneNumber1 - x1 * zoneNumber00);
-        int zoneLambda0;
-        int zoneLambda1;
-
-        if (zoneLambda < 0) {
-            zoneLambda0 = zoneLambda + zoneNumber00;
-            zoneLambda1 = zoneLambda + zoneNumber1;
-        } else {
-            zoneLambda0 = zoneLambda;
-            zoneLambda1 = zoneLambda;
-        }
+        int zoneLambda0 = (zoneLambda < 0) ? zoneLambda + zoneNumber00 : zoneLambda;
+        int zoneLambda1 = (zoneLambda < 0) ? zoneLambda + zoneNumber1 : zoneLambda;
 
         double deltaLambda0 = ((double) 1) / zoneNumber00;
         double deltaLambda1 = ((double) 1) / zoneNumber1;
 
-        double longitude0Turn;
-        double longitude1Turn;
-
-        if (zoneNumber00 == 1) {
-            longitude0Turn = recenterPosition(x0);
-            longitude1Turn = recenterPosition(x1);
-        } else {
-            longitude0Turn = recenterPosition(deltaLambda0 * (zoneLambda0 + x0));
-            longitude1Turn = recenterPosition(deltaLambda1 * (zoneLambda1 + x1));
-        }
+        double longitude0Turn = (zoneNumberOf(a0) == 1) ? recenterPosition(x0) : recenterPosition(deltaLambda0 * (zoneLambda0 + x0));
+        double longitude1Turn = (zoneNumberOf(a0) == 1) ? recenterPosition(x1) : recenterPosition(deltaLambda1 * (zoneLambda1 + x1));
 
         int longitude0T32 = convertTurnToT32(longitude0Turn);
         int longitude1T32 = convertTurnToT32(longitude1Turn);
 
-        if (mostRecent == 0) {
-            return new GeoPos(longitude0T32, latitude0T32);
-        } else {
-            return new GeoPos(longitude1T32, latitude1T32);
-        }
+        return (mostRecent == 0) ? new GeoPos(longitude0T32, latitude0T32) : new GeoPos(longitude1T32, latitude1T32);
     }
 
     private static double aOf(double latitude_TURN) {
@@ -111,19 +83,13 @@ public class CprDecoder {
     }
 
     private static int zoneNumberOf(double A) {
-        if (Math.abs(A) > 1) {
-            return 1;
-        } else {
-            return (int) Math.floor((Math.PI * 2.0) / Math.acos(A));
-        }
+        if (Math.abs(A) > 1) return 1;
+        else return (int) Math.floor((Math.PI * 2.0) / Math.acos(A));
     }
 
     private static double recenterPosition(double latitudeOrLongitude_TURN) {
-        if (latitudeOrLongitude_TURN >= 0.5) {
-            return latitudeOrLongitude_TURN - 1;
-        } else {
-            return latitudeOrLongitude_TURN;
-        }
+        if (latitudeOrLongitude_TURN >= 0.5) return latitudeOrLongitude_TURN - 1;
+        else return latitudeOrLongitude_TURN;
     }
 
     private static int convertTurnToT32(double latitudeOrLongitude_TURN) {
