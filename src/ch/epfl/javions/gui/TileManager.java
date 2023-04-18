1d63804cecfbcd427193d80def0eea867d29ea87
Yshai <yshai.dinee-baumgarten@epfl.ch>
2023-04-18 08:47:43 +0200

    Commit

--- src/ch/epfl/javions/gui/TileManager.java
+++ src/ch/epfl/javions/gui/TileManager.java
@@ -1,12 +1,15 @@
 package ch.epfl.javions.gui;
 
+
 public final class TileManager
 {
+    private static final int MIN_ZOOM = 0;
+    private static final int MAX_ZOOM = 19;
     public record TileId(int zoom, int x, int y)
     {
         public static boolean isValid(int zoom, int x, int y)
         {
-            return true;
+            return (MIN_ZOOM <= zoom && zoom <= MAX_ZOOM);
         }
     }
 }
