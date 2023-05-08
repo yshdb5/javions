fc407e436200010f91c280a8072095825eddeb25
Yshai <yshai.dinee-baumgarten@epfl.ch>
2023-05-08 18:27:35 +0300

    Commit

--- src/ch/epfl/javions/gui/Main.java
+++ src/ch/epfl/javions/gui/Main.java
@@ -1,6 +1,7 @@
 package ch.epfl.javions.gui;
 
 import ch.epfl.javions.ByteString;
+import ch.epfl.javions.Units;
 import ch.epfl.javions.adsb.Message;
 import ch.epfl.javions.adsb.MessageParser;
 import ch.epfl.javions.adsb.RawMessage;
@@ -9,6 +10,7 @@ import ch.epfl.javions.demodulation.AdsbDemodulator;
 import javafx.animation.AnimationTimer;
 import javafx.application.Application;
 import javafx.beans.binding.Bindings;
+import javafx.beans.property.LongProperty;
 import javafx.beans.property.ObjectProperty;
 import javafx.beans.property.SimpleObjectProperty;
 import javafx.collections.ObservableSet;
@@ -23,11 +25,12 @@ import java.io.*;
 import java.net.URL;
 import java.net.URLDecoder;
 import java.nio.file.Path;
+import java.time.Duration;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Objects;
 import java.util.concurrent.ConcurrentLinkedQueue;
 
-import static com.sun.javafx.scene.control.skin.Utils.getResource;
 import static java.nio.charset.StandardCharsets.UTF_8;
 
 public final class Main extends Application {
@@ -37,6 +40,7 @@ public final class Main extends Application {
     public static final int X = 33_530;
     public static final int Y = 23_070;
     private final ConcurrentLinkedQueue<RawMessage> messageQueue = new ConcurrentLinkedQueue<>();
+    private final long startTime = System.nanoTime();
     public static void main(String[] args) {
         launch(args);
     }
@@ -46,7 +50,7 @@ public final class Main extends Application {
         Path tileCache = Path.of("tile-cache");
         TileManager tileManager = new TileManager(tileCache, "tile.openstreetmap.org");
         MapParameters mapParameters = new MapParameters(INITIAL_ZOOM, X, Y);
-        BaseMapController mapController = new BaseMapController(tileManager, mapParameters);
+        BaseMapController baseMap = new BaseMapController(tileManager, mapParameters);
 
         URL url = getClass().getResource("/aircraft.zip");
         assert url != null;
@@ -57,16 +61,16 @@ public final class Main extends Application {
         ObservableSet<ObservableAircraftState> observableAircraftSet = stateManager.states();
         ObjectProperty<ObservableAircraftState> aircraftStateProperty = new SimpleObjectProperty<>();
 
-        AircraftController controller =
+        AircraftController aircraftController =
                 new AircraftController(mapParameters, observableAircraftSet, aircraftStateProperty);
         AircraftTableController tableController =
                 new AircraftTableController(observableAircraftSet, aircraftStateProperty);
 
         StatusLineController lineController = new StatusLineController();
         lineController.aircraftCountProperty().bind(Bindings.size(observableAircraftSet));
-        lineController.messageCountProperty().set(0);
+        LongProperty messageCountProperty = lineController.messageCountProperty();
 
-        StackPane stackPane = new StackPane(mapController.pane(), controller.pane());
+        StackPane stackPane = new StackPane(baseMap.pane(), aircraftController.pane());
         BorderPane borderPane = new BorderPane(
                 tableController.pane(), lineController.pane(), null, null,  null);
 
@@ -96,18 +100,20 @@ public final class Main extends Application {
             {
                 readAllMessages(args.get(0));
             }
-        });
-
-        Iterator<RawMessage> messageIterator = messageQueue.iterator();
+        }).start();
 
         new AnimationTimer() {
             @Override
             public void handle(long now) {
                 try {
-                    for (int i = 0; i < 10; i += 1) {
-                        if (!messageIterator.hasNext()) return;
-                        Message message = MessageParser.parse(messageIterator.next());
-                        if (message != null) stateManager.updateWithMessage(message);
+                    for (int i = 0; i < 10; i++) {
+                        RawMessage rawMessage = messageQueue.poll();
+                        if (rawMessage == null) return;
+                        Message message = MessageParser.parse(rawMessage);
+                        if (message != null) {
+                            stateManager.updateWithMessage(message);
+                            messageCountProperty.set(messageCountProperty.get() + 1);
+                        }
                         stateManager.purge();
                     }
                 } catch (IOException e) {
@@ -118,21 +124,23 @@ public final class Main extends Application {
     }
 
     private void readAllMessages(String fileName){
-        String f = getResource(fileName).getFile();
+        String f = getClass().getResource(fileName).getFile();
         f = URLDecoder.decode(f, UTF_8);
 
-        try (DataInputStream s = new DataInputStream(
+        try (DataInputStream stream = new DataInputStream(
                 new BufferedInputStream(new FileInputStream(f)))){
 
             byte[] bytes = new byte[RawMessage.LENGTH];
-            while (s.available() > 0) {
-                long timeStampNs = s.readLong();
-                int bytesRead = s.readNBytes(bytes, 0, bytes.length);
+            while (stream.available() > 0) {
+                long timeStampNs = stream.readLong();
+                int bytesRead = stream.readNBytes(bytes, 0, bytes.length);
                 assert bytesRead == RawMessage.LENGTH;
                 ByteString message = new ByteString(bytes);
+                //TODO: verifier comment bien faire ca
+                //while (System.nanoTime() < timeStampNs + startTime) Thread.sleep(Duration.ofNanos(1).toMillis());
                 messageQueue.add(new RawMessage(timeStampNs, message));
             }
-        }catch (IOException e) {
+        }catch (IOException /*| InterruptedException*/ e) {
             throw new RuntimeException(e);
         }
     }
